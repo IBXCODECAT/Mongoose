6d532ebf0d5eed38b40c82fa55c9cbb8fd42bf57
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-11-01 15:08:00 -0500

    Add put route

--- server.js
+++ server.js
@@ -216,6 +216,36 @@ app.post(
     }
 )
 
+app.put(
+    'api/posts:id',
+    auth,
+    async (req, res) => {
+        try {
+            const {title, body} = req.body;
+            const Post = await Post.findById(req.params.id);
+
+            if(!post) {
+                return res.status(404).json({msg: 'Post not found'});
+            }
+
+            if(post.user.toString() !== req.user.id) {
+                return res.status(401).json({msg: 'User not authorized'});
+            }
+
+            post.title = title || post.title;
+            post.body = body || post.body;
+
+            await post.save();
+
+            res.json(post);
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Server Error');
+        }
+    }
+)
+
 /**
  * @route DELETE api/posts:id
  * @description Update a post
